e53af19864753ce51f49044dc6e07bef3b4081cb
cristalcorp <cristal@cristalcorp.com>
2025-01-28 20:45:51 +0100

    count letters

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 
+def count_letters(s: str):
+    letters = {}
+    for char in s:
+        if char.lower() not in letters :
+            letters[char.lower()] = 0
+        letters[char.lower()] += 1
+    return letters
+
+
 def main():
 
     file: str = "books/frankenstein.txt"
 
     with open(file) as f:
         file_content = f.read()
-    print(file_content)
+    #print(len(file_content.split()))
+    print(count_letters(file_content))
 
 main()
