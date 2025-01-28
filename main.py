754c7d9c9561ec3074d6bf3376073b4edfe81059
cristalcorp <cristal@cristalcorp.com>
2025-01-28 21:06:22 +0100

    print final report

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def count_letters(s: str):
         letters[char.lower()] += 1
     return letters
 
+def print_char_count(chars: dict[str, int]) -> None :
+    for k, v in chars.items() :
+        if k.isalpha() :
+            print(f"The '{k}' character was found {v} times")
+
+def print_word_count(s: str) -> None :
+    print(f"{len(s.split())} words found in the document")
+
+def print_full_report(s: str, file: str) -> None:
+    print(f"--- Begin report of {file} ---")
+    print_word_count(s)
+    print("")
+    print_char_count(count_letters(s))
+    print("--- End report ---")
 
 def main():
 
@@ -14,7 +28,6 @@ def main():
 
     with open(file) as f:
         file_content = f.read()
-    #print(len(file_content.split()))
-    print(count_letters(file_content))
 
+    print_full_report(file_content, file)
 main()
